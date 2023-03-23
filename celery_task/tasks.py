b9071fcd81c9be24f47d1eb9a5d8d69b2cdf6c91
Mohammed Raihan Ullah <raihan@Mohammeds-MacBook-Pro.local>
2023-03-23 17:13:16 +0600

    made session thread safe

--- celery_task/tasks.py
+++ celery_task/tasks.py
@@ -5,13 +5,17 @@ from schemas import CreateAndUpdateGazeInfo
 from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from database import get_db
+from models import GazeInfo
 
 
-# session: Session = Depends(get_db)
 @shared_task(bind=True,autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 5},
              name='gaze_infos:create_gaze_info_tasks')
-def create_gaze_info_tasks(self, gaze_info: List[CreateAndUpdateGazeInfo] , session : Session = next(get_db())) -> str:
+def create_gaze_info_tasks(self, gaze_info: List[CreateAndUpdateGazeInfo], session : Session = next(get_db())) -> str:
     for tempData in gaze_info:
-        gaze_data = create_gaze_info(session, tempData)
-    # session.commit()
+        # gaze_data = create_gaze_info(session, tempData)
+        new_gaze_info = GazeInfo(**tempData.dict())
+        session.add(new_gaze_info)
+    # session : Session =  get_db()
+    # session.bulk_save_objects(gaze_info)
+    session.commit()
     return "task executed in db"
--- database.py
+++ database.py
@@ -1,19 +1,23 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 from config import DB_URL
+import threading
 
 DATABASE_URL = DB_URL
 
 db_engine = create_engine(DATABASE_URL, echo = False,  pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=db_engine)
 
+# Create a thread-local session
+session_factory = scoped_session(SessionLocal, scopefunc=threading.current_thread)
+
 Base = declarative_base()
 
 def get_db():
     db = None
     try:
-        db = SessionLocal()
+        db = session_factory()
         yield db
     finally:
         db.close()
